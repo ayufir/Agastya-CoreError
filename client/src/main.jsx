ea26ea94e023b441bbd71eb2ba3864f28b8e107d
Ayush <ayush.ayu079@gmail.com>
2026-06-17 15:08:48 +0530

    chore: add developer credit signature

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,11 @@ import { store } from "./redux/store.js";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 
+console.log(
+  "%c💻 Developed by: Ayush Shrivastava", 
+  "color: #6366f1; font-weight: bold; font-size: 13px; font-family: sans-serif; padding: 2px 4px;"
+);
+
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
